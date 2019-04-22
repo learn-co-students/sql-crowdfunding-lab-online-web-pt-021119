15eb1b11d61c052030ca61db4973716f7cd4c905
chilbert <chase@chasehilbert.com>
2019-04-21 21:02:11 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,38 @@
+INSERT INTO projects (id, title, category, funding_goal, start_date, end_date) VALUES (1, "A New You", "Self Help", 300, "12-02-2018", "06-04-2019"), (2, "Cheese Maker", "Food", 250, "11-02-2018", "07-04-2019"), (3, "STEM Computer", "Self Help", 10000, "09-02-2018", "08-04-2019"),
+ (4, "Blender Kit", "Food", 5000, "08-02-2018", "09-04-2019"), (5, "Soccer Goal", "Self Help", 1000, "07-02-2018", "10-04-2019"), (6, "Smart Shoes", "Clothes", 600, "12-12-2018", "11-04-2019"),
+(7, "Memorization Help", "Self Help", 450, "12-16-2018", "12-04-2019"), (8, "Coin", "Money", 150, "12-22-2018", "06-14-2019"), (9, "Speaker Sneaker", "Clothes", 23000, "12-28-2018", "06-25-2019"), (10,"Chip Maker", "Food", 500000, "12-25-2018", "06-08-2019");
+
+INSERT INTO users (id, name, age) VALUES (1, "John Smith", 25), (2, "Jake Smith", 50), (3, "Chelsea Smith", 20), (4, "Bob Smith", 35), (5, "Jim Smith", 45), (6, "Jacke Smith", 55), (7, "Jared Smith", 65),
+(8, "Chase Smith", 75), (9, "Kristine Smith", 125), (10, "Eisley Smith", 525), (11, "Ellen Smith", 725), (12, "Justin Smith", 1025), (13, "John Jackson", 1225), (14, "Jeff Smith", 25),
+(15, "Santa Smith", 125), (16, "Summer Smith", 525), (17, "Adam Smith", 325), (18, "Kelsey Smith", 4525), (19, "Ben Smith", 325), (20, "Pat Smith", 215);
+
+INSERT INTO pledges (id, amount, user_id, project_id) VALUES (1, 50, 1, 3);
+INSERT INTO pledges VALUES (2,50, 1, 3);
+INSERT INTO pledges VALUES (3, 50, 1, 3);
+INSERT INTO pledges VALUES (4, 50, 1, 3);
+INSERT INTO pledges VALUES (5, 50, 1, 3);
+INSERT INTO pledges VALUES (6, 50, 1, 3);
+INSERT INTO pledges VALUES (7, 50, 1, 3);
+INSERT INTO pledges VALUES (8, 50, 1, 3);
+INSERT INTO pledges VALUES (9, 50, 1, 3);
+INSERT INTO pledges VALUES (10, 50, 1, 3);
+INSERT INTO pledges VALUES (11, 50, 1, 3);
+INSERT INTO pledges VALUES (12, 50, 1, 3);
+INSERT INTO pledges VALUES (13, 50, 1, 3);
+INSERT INTO pledges VALUES (14, 50, 1, 3);
+INSERT INTO pledges VALUES (15, 50, 1, 3);
+INSERT INTO pledges VALUES (16, 50, 1, 3);
+INSERT INTO pledges VALUES (17, 50, 1, 3);
+INSERT INTO pledges VALUES (18, 50, 1, 3);
+INSERT INTO pledges VALUES (19, 50, 1, 3);
+INSERT INTO pledges VALUES (20, 50, 1, 3);
+INSERT INTO pledges VALUES (21, 50, 1, 3);
+INSERT INTO pledges VALUES (22, 50, 1, 3);
+INSERT INTO pledges VALUES (23, 50, 1, 3);
+INSERT INTO pledges VALUES (24, 50, 1, 3);
+INSERT INTO pledges VALUES (25, 50, 1, 3);
+INSERT INTO pledges VALUES (26, 50, 1, 3);
+INSERT INTO pledges VALUES (27, 50, 1, 3);
+INSERT INTO pledges VALUES (28, 50, 1, 3);
+INSERT INTO pledges VALUES (29, 50, 1, 3);
+INSERT INTO pledges VALUES (30, 50, 1, 3);
